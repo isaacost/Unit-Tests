450cf4a0ac98807d320236ef124b88b4cc9cc9c3
Isa Costa <isa-hcosta@hotmail.com>
2022-07-19 17:31:33 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  let estudante = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+
+  return estudante;
+};
 
 module.exports = createStudent;
